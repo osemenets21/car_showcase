98939b57575da566a47ffc50552541c75db23645
osemenets21 <oleg.min.vin@gmail.com>
2024-08-07 18:50:10 +0200

    do search field and filter for car catalog

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 
-
 const Hero = () => {
   const handleScroll = () => {
     const nextSection = document.getElementById("discover");
@@ -35,7 +34,6 @@ const Hero = () => {
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
-
         <div className="hero__image-overlay" />
       </div>
     </div>
