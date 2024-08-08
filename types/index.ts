9d2682e8878ab09d36ae77d4bdfa0bdf5a4dc0c7
osemenets21 <oleg.min.vin@gmail.com>
2024-08-08 17:26:12 +0200

    update SearchManufacturer component

--- types/index.ts
+++ types/index.ts
@@ -9,5 +9,5 @@ export interface CustomButtonProps {
 
 export interface SearchManufacturerProps {
     manufacturer: string;
-    setManufacturer: (manufacturer: string) => void;
+    setManuFacturer: (manufacturer: string) => void;
 }
