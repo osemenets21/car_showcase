b538b7af60a64478520301b89a5bc4cdc563b226
osemenets21 <oleg.min.vin@gmail.com>
2024-08-12 18:23:19 +0200

    create CardDetails component and do logic

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
-                <CarCard car={car} />
+                <CarCard car={car} key={car.id} />
               ))}
             </div>
           </section>
